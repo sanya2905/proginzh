9e120c0fca233d206cc232171e6f812886f7de44
sanya2905 <114611289+sanya2905@users.noreply.github.com>
2022-11-14 18:12:29 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,43 @@
+
 #include "Header.h"
 int main()
 {
-	int x = 200000000;
-	unsigned int y = x << 16;
-	unsigned int z = y >> 24;
-	printf("%d", z);
+	char a[] = "privet vsem ya sasha";
+	int i,k;
+	int count_probel = 0;
+	int length = strlen(a);
+	for (i = 0; i < length; i++)
+	{
+		printf("%c", a[i]);
+	}                 
+	printf("\n");
+	for (i = length; i >=0; i--) 
+	{
+		printf("%c", a[i]);
+	}
+	printf("\n");
+	int k = 0;
+	int count_probel;
+	for (i = 0; i < length; i++)
+	{
+		if (a[i] == ' ') {
+			count_probel++;
+		}
+	}
+	printf("%d \n", count_probel);
+	for (i = 0; i < length; i++)
+	{
+		while (a[i] != ' ') {
+			printf("%c", a[i]);
+			i++;
+			k = i;
+		}
+		printf("\n");
+		for (k; k < length; k++)
+		{
+			printf("%c", a[k+1]);
+		}
+		break;
+	}
 	return 0;
 }
